c360d69d49faa68b85c89007b3cf78c556c8a3be
Uriel <uriel_goncalves@hotmail.com>
2019-08-11 23:36:34 +0100

    Criada a aplicaçao e inicio da tela de login

---
+++ tindev/src/pages/Login.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { StyleSheet, View, Image, Text, TextInput, TouchableOpacity } from 'react-native';
+import logo from '../assets/logo.png';
+
+export default function Login() {
+    return (
+        <View style={styles.container}>
+            <Image source={logo} />
+            <TextInput
+                placeholder="Digite seu usuário no GitHub"
+                placeholderTextColor="#999"
+                style={styles.input}
+            />
+
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}></Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F5F5F5',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 30
+    },
+    input: {
+        height: 46,
+        alignSelf: 'stretch',
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: '#DDD',
+        borderRadius: 4,
+        marginTop: 20,
+        paddingHorizontal: 15,
+    },
+    button: {
+        height: 46,
+        alignSelf: 'stretch',
+        backgroundColor: '#DF4723',
+        borderRadius: 4,
+        marginTop: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 16
+    }
+});
